346e46fe0e05ef66f5e660e5c068dc85d53f7730
bypass <150051173+SAGAEXM@users.noreply.github.com>
2023-11-23 01:28:48 +0530

    Update Tutul16.py

--- Tutul16.py
+++ Tutul16.py
@@ -667,7 +667,7 @@ def passwrd():
                     pwv.append(nmf)
                     pwv.append(frs'123')
                     pwv.append(frs+'@123')
-                    pwv.append(frs+'@')
+                    pwv.append('57575751')
                     pwv.append(frs+'@1234'
             if 'ya' in pwpluss:
                 for xpwd in pwnya:
